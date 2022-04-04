0686835be8f00b0dc8f74e6c0b756b34d1d17c3f
DzejlanRedzepefendic <dzejlanven@icloud.com>
2022-04-04 13:21:09 +0200

    decode jwt

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-function createJwtToken(userInfo: any, secret: string, time: string = '30d') {
+function createJwtToken(userInfo: any, secret: string, time: string = "30d") {
   return jwt.sign(userInfo, secret, { expiresIn: time });
 }
 
@@ -8,4 +8,7 @@ function verifyJwtToken(signedToken: string, secret: string) {
   return jwt.verify(signedToken, secret);
 }
 
-export { createJwtToken, verifyJwtToken };
+function decodeJwtToken(signedToken: string) {
+  return jwt.decode(signedToken);
+}
+export { createJwtToken, verifyJwtToken,decodeJwtToken };
